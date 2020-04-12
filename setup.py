1797b01a768d3fcf0a0a795c40af69738b18b05b
Giannis Daras <daras.giannhs@gmail.com>
2020-04-12 19:58:23 +0300

    Manually assign version number

--- setup.py
+++ setup.py
@@ -23,18 +23,10 @@ with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
     README = f.read()
 
 
-def get_version():
-    main_file = os.path.join(CURDIR, "kmeans_pytorch", "main.py")
-    _version_re = re.compile(r"__version__\s+=\s+(?P<version>.*)")
-    with open(main_file, "r", encoding="utf8") as f:
-        match = _version_re.search(f.read())
-        version = match.group("version") if match is not None else '"unknown"'
-    return str(ast.literal_eval(version))
-
 
 setup(
     name="balanced_kmeans",
-    version=get_version(),
+    version=0.0.1,
     author="@giannisdaras",
     author_email="daras.giannhs@gmail.com",
     description="Balanced kmeans with cuda support in Pytorch.",
